9bab0b08bcd81fb341cccd250f0fc6b82f8d2861
Zsolt Tasnadi <zsolt.tasnadi@zenheads.hu>
2025-12-04 12:54:45 +0100

    python html server for web export

---
+++ serve.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Simple static file server for Bomberman HTML export."""
+
+import http.server
+import socketserver
+import os
+import webbrowser
+
+PORT = 3333
+DIRECTORY = "bomberman"
+
+os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), DIRECTORY))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    url = f"http://localhost:{PORT}"
+    print(f"Serving at {url}")
+    webbrowser.open(url)
+    httpd.serve_forever()
